c8c5fc63587e1ae8c6565964d75d804480beb5f7
qxzhoupku <121421249+qxzhoupku@users.noreply.github.com>
2024-07-31 13:20:44 +0200

    Optimize data save

--- parameters.py
+++ parameters.py
@@ -2,18 +2,18 @@ import numpy as np
 import os
 import time
 
-mode_number = 2**10
+mode_number = 2**8
 iter_number = 10**6
 plot_interval = 5000
 record_interval = iter_number // 10000
 # zeta is changing every single iteration
-zeta_ini = -0.5 - 0.0001
-zeta_end = -0.5 + 0.0001
+zeta_ini = -10 - 0.0001
+zeta_end = +30 + 0.0001
 zetas = np.linspace(zeta_ini, zeta_end, iter_number)
 
-f_A = 3
+f_A = 9
 f_B = 0
-J_back_r = 2.85
+J_back_r = 2.85 / 2
 # loss_back = 1
 # r_back = loss_back * J_back_r**0.5; t_back = loss_back * (1 - J_back_r)**0.5
 delta_t = 0.0001 # commonly used time step
@@ -31,7 +31,7 @@ os.chdir("../output")
 # save all the variables to a file
 with open(f"{time_str}.txt", 'w') as file:
     for var in dir():
-        if var in {"np", "file", "time"}:
+        if var in {"np", "file", "time" "D_int"}:
             continue
         if not var.startswith("__") and not var.startswith("_"):
             file.write(f"{var} = {eval(var)}\n")
