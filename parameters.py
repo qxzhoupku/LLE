6629fb9e2f6407b23bceabc5935d3d9ac5b066ff
qxzhoupku <121421249+qxzhoupku@users.noreply.github.com>
2024-08-27 11:59:37 +0200

    chore: Remove colorbars in outputs from viewer.py

--- parameters.py
+++ parameters.py
@@ -3,20 +3,20 @@ import os
 import time
 
 mode_number = 2**8
-iter_number = 10**7
+iter_number = 10**8
 plot_interval = 5000
 record_interval = iter_number // 10000
 power_interval = max(iter_number // 1000000, 1)
 # zeta is changing every single iteration
-zeta_ini = -5 - 0.0001
-zeta_end = +15 + 0.0001
+zeta_ini = -4 - 0.0001
+zeta_end = +6 + 0.0001
 zeta_step = (zeta_end - zeta_ini) / (iter_number - 1)
 
-f_A = 8
+f_A = 5
 f_B = 0
 delta_t = 1e-4 # commonly used time step
 delta_t = 1e-5
-J_back_r = 2.85
+J_back_r = 4.85
 
 noise_level = 1e-6
 
