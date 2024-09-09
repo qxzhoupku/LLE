d1140929ef22d52969e63a75fa9da6f85cff9f7e
qxzhoupku <121421249+qxzhoupku@users.noreply.github.com>
2024-09-09 23:50:20 +0200

    Modify viewer_anytime.py: Add power figure

--- viewer_anytime.py
+++ viewer_anytime.py
@@ -11,6 +11,9 @@ time_str = sys.argv[1]
 os.chdir(os.path.dirname(__file__))
 os.chdir("../cache/saved")
 
+
+record_power_A = np.load(f"{time_str}_record_power_A.npy")
+record_power_B = np.load(f"{time_str}_record_power_B.npy")
 record_waveform_A = np.load(f"{time_str}_record_waveform_A.npy")
 record_waveform_B = np.load(f"{time_str}_record_waveform_B.npy")
 record_freq_A = np.fft.fftshift(np.fft.fft(record_waveform_A, axis=1), axes=1)
@@ -19,9 +22,16 @@ time_str, f_A, f_B, J_back_r, mode_number, zeta_ini, zeta_end, iter_number, powe
 xs_freq = np.arange(-mode_number / 2, mode_number / 2)
 zetas = np.load(f"{time_str}_zetas.npy")
 length_zetas = len(zetas)
-
 length = len(record_waveform_A)
 print(f"Valid range: [0, {length})")
+
+plt.figure().set_size_inches(5, 7)
+plt.plot(zetas, record_power_A, label=f'Power A, f_A = {f_A}', alpha = 0.7)
+plt.plot(zetas, record_power_B, label=f'Power B, f_B = {f_B}', alpha = 0.7)
+plt.xlim(zeta_ini, zeta_end)
+plt.title(f"Power, J = {J_back_r}")
+plt.xlabel("detuning")
+plt.legend(loc = "lower left")
 
 fig, axs = plt.subplots(2)
 fig.set_size_inches(5, 7)
@@ -56,13 +66,14 @@ ax_freq.set_ylim(0.3 * min(np.abs(record_freq_A[-1])[0], np.abs(record_freq_B[-1
 
 line_A, = ax.plot(np.abs(record_waveform_A[0]), alpha=0.7)
 line_B, = ax.plot(np.abs(record_waveform_B[0]), alpha=0.7)
+ax.set_title(f"Iteration: {0}, detuning: {zetas[0]:.2f}")
 line_A_freq, = ax_freq.plot(xs_freq, np.abs(record_freq_A[0]), alpha=0.7)
 line_B_freq, = ax_freq.plot(xs_freq, np.abs(record_freq_B[0]), alpha=0.7)
 
 def update(val):
     iter = int(slider.val)
     detuning = zetas[iter * length_zetas // length]
-    fig.suptitle(f"Iteration: {iter}, detuning: {detuning:.2f}")
+    ax.set_title(f"Iteration: {iter}, detuning: {detuning:.2f}")
     line_A.set_ydata(np.abs(record_waveform_A[iter]))
     line_B.set_ydata(np.abs(record_waveform_B[iter]))
     line_A_freq.set_ydata(np.abs(record_freq_A[iter]))
